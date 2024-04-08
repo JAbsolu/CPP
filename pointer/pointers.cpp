820c5ce98b97aad1dbab08c599b5a63f4172a69c
John Absolu <Johnson.absolu@hotmail.com>
2024-04-08 17:43:34 -0400

    Update | Learning C++ Pointers

--- pointer/pointers.cpp
+++ pointer/pointers.cpp
@@ -7,11 +7,24 @@ void pointers(int a){
   (a)++;
   int num = 1;
   int *numPointer = &num;
-  cout << "This is the pointer for the number: " << a << " " << &a << "\nThis is the pointer for the number: " << num << " " << numPointer << endl;
+  cout << "\nThis is the pointer for the number: " << a << " " << &a;
+  cout <<  "\nThis is the pointer for the number: " << num << " " << numPointer << endl << endl; 
+}
+
+void printPointer(string str) {
+  char firstChar = str[0];
+  char *fCPointer = &firstChar;
+  string *stringPointer = &str;
+
+  cout <<  "The string is: " << str << endl;
+  cout << "The first character in the string is: " << firstChar << endl;
+  cout << "The first character's pointer is: " << fCPointer << endl;
+  cout << "The string's pointer is: " << stringPointer << endl << endl;
 }
 
 int main() {
   int a = 10;
   pointers(a);
+  printPointer("John");
 }
 
