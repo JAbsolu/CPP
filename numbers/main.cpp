43373475307d000d2f08e18841a4915248cc9ceb
John Absolu <Johnson.absolu@hotmail.com>
2024-04-08 12:25:48 -0400

    Added numbers

---
+++ numbers/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void add(int a, int b) {
+  cout << a + b;
+}
+
+void multiply(int a, int b) {
+  cout << a * b;
+}
+
+void subtract(int a, int b) {
+  cout << a - b;
+}
+
+void divide(double a, double b) {
+  cout << a / b;
+}
+
+int main(){
+  add(4, 6);
+  multiply(4,5);
+  subtract(10,3);
+  divide(5.00, 2.45);
+}
