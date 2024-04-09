c3c50b8aa048a19708268d7a2897bbc9113e9118
John Absolu <Johnson.absolu@hotmail.com>
2024-04-09 11:49:36 -0400

    Update: More Pointers | Learning C++

---
+++ charPlay/solution.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+#include <cstring>  
+
+/**
+ * Create a function definition that returns a character pointer to an array containing the alpha characters 
+ * of a passed character array in upper case and in alphabetical order.
+char *phrase = new char [271 {"Once more into the breach!".
+char *phrase alphas = alpha in (phrase); //contains: "ABCEHIMNORT\O"
+6. Create a function definition that is passed a pointer to a character array and returns a vector containing the string with its vowels removed and the removed vowels.
+char *name = new char [11] {"Fred Smith"};
+std::vector<std::string> dv vct = disemvoweled (name);
+std:: cout << dv vet lOl « "In':
+/displays: Frd Smth
+std:: cout << dv vet [1] < "In'; I/displays: ei
+*/
+
+/**
+ * 1. Create a nondiscard function that returns a character pointer to an arr
+ * The array will contain alpha characters of a character array. the characters will be in upper case and in
+ * alphabetical order (a - z)
+ * 
+ * 2. 
+*/
+
+[[nodiscard]] char alphabetIn(char * arr){
+
+}
